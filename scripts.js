04fc35e9a6b7e068a904ecdbf70294721392777c
Alex Werner <alexwerner569@hotmail.com>
2022-09-12 21:57:00 -0700

    working on functionality to change grid size.

--- scripts.js
+++ scripts.js
@@ -1,25 +1,34 @@
 const container = document.getElementById('container')
+const button = document.getElementById('submit')
 
 
 function makeRows(rows, cols){
+    
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for ( let c = 0; c < (rows * cols); c++){
         let cell = document.createElement('div');
-        cell.innerText = (c+1);
+        
         container.appendChild(cell).className= 'grid'
     }
 }
 
-makeRows(50,50);
+makeRows(80,80);
+
+
+
+button.addEventListener('click', () => {
+    const xaxis = document.getElementById('xaxis').value
+    const yaxis = document.getElementById('yaxis').value
+    container.innerHTML = ""
+    makeRows(xaxis,yaxis)
+})
+
+
 
 pieces = document.querySelectorAll('.grid')
-
 pieces.forEach((piece) => {
     piece.addEventListener('mouseover', function(e){
         piece.classList.add('hovered')
     });
 });
-
-
-
